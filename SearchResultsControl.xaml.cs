1c1ecd8a6bafd676751fadf88393ad9dcedb50b3
test name <test email>
2026-05-11 18:35:10 +0300

    searchbar

---
+++ SearchResultsControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using Messenger_Project.Models;
+
+namespace Messenger_Project
+{
+    public partial class SearchResultsControl : UserControl
+    {
+        // Конструктор приймає список знайдених юзерів з БД
+        public SearchResultsControl(List<User> foundUsers)
+        {
+            InitializeComponent();
+            ResultsListBox.ItemsSource = foundUsers; 
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            // Закриваємо сторінку результатів і повертаємо чат
+            if (Window.GetWindow(this) is MainWindow mainWindow)
+            {
+                mainWindow.MainArea.Content = null;
+                mainWindow.ChatView.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
